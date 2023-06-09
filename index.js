cbd68815414d187175712e23ae6e3c8b0cfe3d05
albvnovs <albvnovs@gmail.com>
2023-06-10 01:16:53 +0600

    login add

--- index.js
+++ index.js
@@ -219,7 +219,7 @@ app.post('/register', async (req, res) => {
   }
 });
 
-app.post('/login', async (req, res) => {
+app.post('/logins', async (req, res) => {
   try {
     const { username, password } = req.body;
 
@@ -241,51 +241,6 @@ app.post('/login', async (req, res) => {
     res.status(500).json({ message: 'Что-то пошло не так' });
   }
 });
-
-export const register = async (req, res) => {
-  try {
-    const { username, password } = req.body;
-
-    const existingUser = await User.findOne({ username });
-    if (existingUser) {
-      return res.status(400).json({ message: 'Пользователь с таким именем уже существует' });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const user = new User({ username, password: hashedPassword });
-    await user.save();
-
-    res.status(200).json({ message: 'Регистрация прошла успешно' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Что-то пошло не так' });
-  }
-};
-
-export const login = async (req, res) => {
-  try {
-    const { username, password } = req.body;
-
-    const user = await User.findOne({ username });
-    if (!user) {
-      return res.status(400).json({ message: 'Неправильное имя пользователя или пароль' });
-    }
-
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) {
-      return res.status(400).json({ message: 'Неправильное имя пользователя или пароль' });
-    }
-
-    const token = jwt.sign({ userId: user._id }, 'secret_key');
-
-    res.status(200).json({ token });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Что-то пошло не так' });
-  }
-};
-
 
 const PORT = 4000;
 
