2c242585797aeb5f19fe6d34c35f85a7524f9263
albvnovs <albvnovs@gmail.com>
2023-06-10 00:12:45 +0600

    deploy version

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import { connect } from './src/db/db.js';
 import authRoutes from './src/routes/authRoutes.js';
-import telegramRoutes from './src/routes/telegramRoutes.js';
-// import instagramRoutes from './src/routes/instagramRoutes.js';
-import whatsappRoutes from './src/routes/whatsappRoutes.js';
 import MyModel from './src/models/MyModel.js';
+import MyModelForTg from './src/models/MyModelForTg.js';
+import MyModelForWA from './src/models/MyModelForWA.js';
 
 const app = express();
 app.use(express.json());
@@ -14,9 +13,6 @@ app.use(cors());
 connect(); // Подключение к базе данных
 
 app.use('/test', authRoutes); // Маршруты аутентификации и регистрации
-app.use('/test/telegramSlot', telegramRoutes); // Маршруты для слотов Telegram
-// app.use('/test/mymodels', instagramRoutes); // Маршруты для модели MyModel
-app.use('/test/whatsappSlot', whatsappRoutes); // Маршруты для модели MyModelForWA
 
 app.post('/test/mymodels', async (req, res) => {
   try {
@@ -74,6 +70,131 @@ app.get('/test/mymodels', async (req, res) => {
     res.status(500).json({
       error: 'Что то пошло не так'
     })
+  }
+});
+
+
+app.post('/telegramslot', async (req, res) => {
+  try {
+    const { account, num } = req.body;
+    const myData = new MyModelForTg({ account, num });
+    await myData.save();
+    res.status(200).json({ message: 'Данные успешно добавлены' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Что-то пошло не так' });
+  }
+});
+
+app.patch('/test/telegramSlot/:id', async (req, res) => {
+  const { id } = req.params;
+  const { monako, lider, fenix, turan } = req.body;
+
+  try {
+    const updatedTelegram = await MyModelForTg.findByIdAndUpdate(
+      id,
+      { monako, lider, fenix, turan },
+      { new: true }
+    );
+    res.json(updatedTelegram);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Ошибка сервера' });
+  }
+});
+
+app.post('/insert/account', async (req, res) => {
+  try {
+    const { account } = req.body;
+
+    for (let i = 1; i <= 10; i++) {
+      const myData = new MyModelForTg({
+        account,
+        num: i,
+        monako: '',
+        fenix: '',
+        lider: '',
+        turan: '',
+      });
+      await myData.save();
+    }
+
+    res.status(200).json({ message: 'Слоты успешно созданы' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Что-то пошло не так' });
+  }
+});
+
+app.get('/test/telegramSlot', async (req, res) => {
+  try {
+    const data = await MyModelForTg.find();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Что-то пошло не так' });
+  }
+});
+
+app.post('/test/whatsappslot', async (req, res) => {
+  try {
+    const { account, num } = req.body;
+    const myData = new MyModelForWA({ account, num });
+    await myData.save();
+    res.status(200).json({ message: 'Данные успешно добавлены' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Что-то пошло не так' });
+  }
+});
+
+app.patch('/test/whatsappslot/:id', async (req, res) => {
+  const { id } = req.params;
+  const { monako, lider, fenix, turan } = req.body;
+
+  try {
+    const updatedWhatsapp = await MyModelForWA.findByIdAndUpdate(
+      id,
+      { monako, lider, fenix, turan },
+      { new: true }
+    );
+    res.json(updatedWhatsapp);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Ошибка сервера' });
+  }
+});
+
+app.post('/test/whatsappslot', async (req, res) => {
+  try {
+    const { account } = req.body;
+
+    for (let i = 1; i <= 20; i++) {
+      const myData = new MyModelForWA({
+        account,
+        num: i,
+        monako: '',
+        fenix: '',
+        lider: '',
+        turan: '',
+      });
+      await myData.save();
+    }
+
+    res.status(200).json({ message: 'Слоты успешно созданы' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Что-то пошло не так' });
+  }
+});
+
+app.get('/test/whatsappslot', async (req, res) => {
+  try {
+    const data = await MyModelForWA.find();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Что-то пошло не так' });
   }
 });
 
