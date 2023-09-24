39fa3b2f1de2951b80d7e0152828d58c0e54547b
albanovs <albvnovs@gmail.com>
2023-09-25 01:02:34 +0600

    done

--- index.js
+++ index.js
@@ -2411,6 +2411,47 @@ app.get("/test/simcardmanagers", async (req, res) => {
   }
 })
 
+app.patch("/test/simcardmanagers/:id", async (req, res) => {
+  const { id } = req.params;
+  const {
+    number,
+    status,
+    buyer,
+    personal_number,
+    date_of_verification,
+    days_since_verification,
+    status_simCard,
+    physical_simCard,
+    registration,
+    WAcod,
+    TGcod,
+  } = req.body
+  try {
+    const updateSimCard = await SimModelMonaco.findOneAndUpdate(
+      { "slot._id": id },
+      {
+        "slot.$.number": number,
+        "slot.$.status": status,
+        "slot.$.buyer": buyer,
+        "slot.$.personal_number": personal_number,
+        "slot.$.date_of_verification": date_of_verification,
+        "slot.$.days_since_verification": days_since_verification,
+        "slot.$.status_simCard": status_simCard,
+        "slot.$.physical_simCard": physical_simCard,
+        "slot.$.registration": registration,
+        "slot.$.WAcod": WAcod,
+        "slot.$.TGcod": TGcod,
+      },
+      { new: true }
+    )
+    res.json(updateSimCard);
+  } catch (error) {
+    res.status(500).json({
+      error: "Что то пошло не так",
+    });
+  }
+})
+
 app.patch('/update/simcardmanagers', async (req, res) => {
   const { itemId, field, value, days_since_verification } = req.body;
 
