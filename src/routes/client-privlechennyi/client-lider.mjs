87ee704b2265b44c15e3862dc1afadc05c2865f9
albanovs <albvnovs@gmail.com>
2023-11-10 23:52:14 +0600

    news id

--- src/routes/client-privlechennyi/client-lider.mjs
+++ src/routes/client-privlechennyi/client-lider.mjs
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 import clientsLider from '../../controllers/clients-privlechennyi/clientsLider.mjs';
 
-router.post('/new-leaderclient:id', clientsLider.newClient)
+router.post('/new-leaderclient/:id', clientsLider.newClient)
 router.get('/leaderclient', clientsLider.getClient)
 
 export default router;
