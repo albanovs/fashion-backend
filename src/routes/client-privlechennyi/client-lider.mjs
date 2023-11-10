f0dbcc16266051997e4fd0b4fbbece43c6b57078
albanovs <albvnovs@gmail.com>
2023-11-11 03:40:07 +0600

    rev

--- src/routes/client-privlechennyi/client-lider.mjs
+++ src/routes/client-privlechennyi/client-lider.mjs
@@ -3,7 +3,7 @@ const router = express.Router();
 import clientsLider from '../../controllers/clients-privlechennyi/clientsLider.mjs';
 
 router.post('/new-leaderclient/:id', clientsLider.newClient)
-router.get('/leaderclient', clientsLider.updateClient)
-router.patch('/leaderclient-update/:id', clientsLider.getClient)
+router.get('/leaderclient', clientsLider.getClient)
+router.patch('/leaderclient-update/:id', clientsLider.updateClient)
 
 export default router;
