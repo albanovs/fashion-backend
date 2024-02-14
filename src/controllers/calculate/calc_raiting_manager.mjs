efb1a301daae128b5e06c8d88072a93daff78f0c
albanovs <albvnovs@gmail.com>
2024-02-14 17:32:47 +0600

    fegrnh

--- src/controllers/calculate/calc_raiting_manager.mjs
+++ src/controllers/calculate/calc_raiting_manager.mjs
@@ -160,7 +160,7 @@ async function calculateAndCacheData() {
         });
 
         let percentVM = 0
-        result.forEach(elem => percentVM += parseFloat(elem.comissionVM))
+        result.forEach(elem => percentVM += parseFloat(elem.comissonVM))
         result.forEach(elem => {
             if (elem.curator.includes("ВМ")) {
                 elem.comission = percentVM
