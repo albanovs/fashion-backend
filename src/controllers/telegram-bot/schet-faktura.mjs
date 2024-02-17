799885304a073d2c8820652dfe5e93eacc9ab3b4
albanovs <albvnovs@gmail.com>
2024-02-17 12:11:45 +0600

    bot tg

--- src/controllers/telegram-bot/schet-faktura.mjs
+++ src/controllers/telegram-bot/schet-faktura.mjs
@@ -1,3 +1,28 @@
 import { Telegraf } from 'telegraf'
 
+const token = "6928660684:AAH8rryO_0FdwaBHuGyKp6z90Rn2dPnrZKY"
 const bot = new Telegraf("6928660684:AAH8rryO_0FdwaBHuGyKp6z90Rn2dPnrZKY")
+
+bot.start((ctx) => {
+    ctx.reply("Привет! Чем могу помочь?");
+    // Отправка клавиатуры с выбором действий
+    ctx.reply("Выберите действие:", {
+        reply_markup: {
+            keyboard: [
+                [{ text: "Создать счет-фактуру" }],
+                [{ text: "Посмотреть статистику" }]
+            ],
+            resize_keyboard: true
+        }
+    });
+});
+
+bot.hears("Создать счет-фактуру", (ctx) => {
+    ctx.reply("Вы выбрали создать счет-фактуру. Далее можно реализовать соответствующую логику.");
+});
+
+bot.hears("Посмотреть статистику", (ctx) => {
+    ctx.reply("Вы выбрали посмотреть статистику. Далее можно реализовать соответствующую логику.");
+});
+
+bot.launch(); 
---
+++ src/models/schet-factura/schet-faktura.mjs
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const schetFacturaSchema = new mongoose.Schema({
+    manager: String,
+    admin: String,
+    status: String,
+    FIO: String,
+    city: String,
+    bank: String,
+});
+
+const schetfakturaModel = mongoose.model('schetfaktura', schetFacturaSchema);
+
+
+export default schetfakturaModel;
