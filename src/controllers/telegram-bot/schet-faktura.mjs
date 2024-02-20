da8da3a82ea66ac5227da98d9e4cf563d1d55e94
albanovs <albvnovs@gmail.com>
2024-02-20 12:14:43 +0600

    выборки для бота

--- src/controllers/telegram-bot/schet-faktura.mjs
+++ src/controllers/telegram-bot/schet-faktura.mjs
@@ -29,7 +29,10 @@ function askAdmin(ctx) {
 }
 
 function askStatus(ctx) {
-    ctx.reply("Введите статус:");
+    ctx.reply("Введите статус или выберите из списка:", Markup.inlineKeyboard([
+        Markup.button.callback('Банк', 'select_status_bank'),
+        Markup.button.callback('Карта', 'select_status_card')
+    ]));
 }
 
 function askFIO(ctx) {
@@ -41,7 +44,16 @@ function askCity(ctx) {
 }
 
 function askBank(ctx) {
-    ctx.reply("Введите банк:");
+    ctx.reply("Введите банк или выберите из списка:", Markup.inlineKeyboard([
+        Markup.button.callback('Байкай банк', 'select_bank_baikay'),
+        Markup.button.callback('Оптима банк', 'select_bank_optima'),
+        Markup.button.callback('Дос-Кредо Банк', 'select_bank_doskredo'),
+        Markup.button.callback('Компаньон Банк', 'select_bank_companion'),
+        Markup.button.callback('Capital Bank', 'select_bank_capital'),
+        Markup.button.callback('Элсом', 'select_bank_elsom'),
+        Markup.button.callback('Финанс Кредит банк', 'select_bank_financecredit'),
+        Markup.button.callback('Кыргызстан банк', 'select_bank_kyrgyzstan')
+    ]));
 }
 
 bot.on('text', (ctx) => {
@@ -89,6 +101,28 @@ bot.action('delete_invoice', (ctx) => {
 bot.action('fill_positions', (ctx) => {
     // Ваш код для заполнения позиций
     ctx.reply('Заполняем позиции.');
+});
+
+bot.action('select_bank_baikay', (ctx) => {
+    state[ctx.from.id].bank = 'Байкай банк';
+    askStatus(ctx);
+});
+
+bot.action('select_bank_optima', (ctx) => {
+    state[ctx.from.id].bank = 'Оптима банк';
+    askStatus(ctx);
+});
+
+// Повторите это для всех вариантов банков
+
+bot.action('select_status_bank', (ctx) => {
+    state[ctx.from.id].status = 'Банк';
+    askFIO(ctx);
+});
+
+bot.action('select_status_card', (ctx) => {
+    state[ctx.from.id].status = 'Карта';
+    askFIO(ctx);
 });
 
 export default bot
