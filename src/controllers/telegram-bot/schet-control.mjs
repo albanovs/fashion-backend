d1b99a734233914545a730240f5b38734236167e
albanovs <albvnovs@gmail.com>
2024-03-27 13:37:27 +0600

    start bot

--- src/controllers/telegram-bot/schet-control.mjs
+++ src/controllers/telegram-bot/schet-control.mjs
@@ -5,6 +5,12 @@ import schetfakturaTuranModel from '../../models/schet-factura/turan-schet.mjs'
 import schetfakturaLibertyModel from '../../models/schet-factura/liberty-schet.mjs'
 import schetfakturaFenixModel from '../../models/schet-factura/ilyas-schet.mjs'
 import schetfakturaYntymakModel from '../../models/schet-factura/yntymak-schet.mjs'
+import monacoOtchetBetaModel from "../../models/monaco/monacoOtchetBeta.mjs";
+import fenixOtchetBetaModel from "../../models/fenix/fenixOtchetBeta.mjs";
+import TuranOtchetBetaModel from "../../models/turan/turanOtchetBeta.mjs";
+import liderOtchetBetaModel from "../../models/lider/liderOtchetBeta.mjs";
+import newOtdelOtchetBetaModel from "../../models/new-otel/newOtdelOtchetBeta.mjs";
+import libertyOtchetBetaModel from "../../models/liberty/libertyOtchetBeta.mjs";
 
 const getSchetData = async (req, res) => {
     try {
@@ -17,6 +23,73 @@ const getSchetData = async (req, res) => {
     }
 }
 
+const getSchetDataleader = async (req, res) => {
+    try {
+        const data = await schetfakturaLeaderModel.find();
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(500).json({
+            error: "Что то пошло не так",
+        });
+    }
+}
+
+const getSchetDatamonaco = async (req, res) => {
+    try {
+        const data = await schetfakturaMonacoModel.find();
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(500).json({
+            error: "Что то пошло не так",
+        });
+    }
+}
+
+const getSchetDataturan = async (req, res) => {
+    try {
+        const data = await schetfakturaTuranModel.find();
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(500).json({
+            error: "Что то пошло не так",
+        });
+    }
+}
+
+const getSchetDatafenix = async (req, res) => {
+    try {
+        const data = await schetfakturaFenixModel.find();
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(500).json({
+            error: "Что то пошло не так",
+        });
+    }
+}
+
+const getSchetDataliberty = async (req, res) => {
+    try {
+        const data = await schetfakturaLibertyModel.find();
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(500).json({
+            error: "Что то пошло не так",
+        });
+    }
+}
+
+const getSchetDatafbox = async (req, res) => {
+    try {
+        const data = await schetfakturaYntymakModel.find();
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(500).json({
+            error: "Что то пошло не так",
+        });
+    }
+}
+
+
 const updateSchetTeam = async () => {
     try {
         const data = await schetfakturaModel.find();
@@ -24,21 +97,136 @@ const updateSchetTeam = async () => {
             switch (item.team) {
                 case 'leader':
                     await schetfakturaLeaderModel.findOneAndUpdate({ _id: item._id }, item, { upsert: true });
+                    const dataotchet = await liderOtchetBetaModel.find();
+                    if (dataotchet.length > 0) {
+                        const firstItem = dataotchet[0];
+                        if (firstItem.otchet && firstItem.otchet.length > 0) {
+                            for (const obj of firstItem.otchet) {
+                                if (!obj.sity && !obj.admin && !obj.comPersent100) {
+                                    obj.sity = `${item.FIO} ${item.city}`;
+                                    obj.admin = item.admin;
+                                    obj.buyer = item.manager;
+                                    obj.comPersent100 = item.comission;
+                                }
+                            }
+                            await firstItem.save();
+                        } else {
+                            console.error('В liderOtchetBetaModel нет массива otchet или он пустой');
+                        }
+                    } else {
+                        console.error('Не найдены данные в liderOtchetBetaModel');
+                    }
                     break;
+
                 case 'monaco':
                     await schetfakturaMonacoModel.findOneAndUpdate({ _id: item._id }, item, { upsert: true });
+                    const dataotchetmonaco = await monacoOtchetBetaModel.find();
+                    if (dataotchet.length > 0) {
+                        const firstItem = dataotchetmonaco[0];
+                        if (firstItem.otchet && firstItem.otchet.length > 0) {
+                            for (const obj of firstItem.otchet) {
+                                if (!obj.sity && !obj.admin && !obj.comPersent100) {
+                                    obj.sity = `${item.FIO} ${item.city}`;
+                                    obj.admin = item.admin;
+                                    obj.buyer = item.manager;
+                                    obj.comPersent100 = item.comission;
+                                }
+                            }
+                            await firstItem.save();
+                        } else {
+                            console.error('В monaco нет массива otchet или он пустой');
+                        }
+                    } else {
+                        console.error('Не найдены данные в monaco');
+                    }
                     break;
                 case 'turan':
                     await schetfakturaTuranModel.findOneAndUpdate({ _id: item._id }, item, { upsert: true });
+                    const dataotchetturan = await TuranOtchetBetaModel.find();
+                    if (dataotchet.length > 0) {
+                        const firstItem = dataotchetturan[0];
+                        if (firstItem.otchet && firstItem.otchet.length > 0) {
+                            for (const obj of firstItem.otchet) {
+                                if (!obj.sity && !obj.admin && !obj.comPersent100) {
+                                    obj.sity = `${item.FIO} ${item.city}`;
+                                    obj.admin = item.admin;
+                                    obj.buyer = item.manager;
+                                    obj.comPersent100 = item.comission;
+                                }
+                            }
+                            await firstItem.save();
+                        } else {
+                            console.error('В turan нет массива otchet или он пустой');
+                        }
+                    } else {
+                        console.error('Не найдены данные в turan');
+                    }
                     break;
                 case 'liberty':
                     await schetfakturaLibertyModel.findOneAndUpdate({ _id: item._id }, item, { upsert: true });
+                    const dataotchetliberty = await libertyOtchetBetaModel.find();
+                    if (dataotchet.length > 0) {
+                        const firstItem = dataotchetliberty[0];
+                        if (firstItem.otchet && firstItem.otchet.length > 0) {
+                            for (const obj of firstItem.otchet) {
+                                if (!obj.sity && !obj.admin && !obj.comPersent100) {
+                                    obj.sity = `${item.FIO} ${item.city}`;
+                                    obj.admin = item.admin;
+                                    obj.buyer = item.manager;
+                                    obj.comPersent100 = item.comission;
+                                }
+                            }
+                            await firstItem.save();
+                        } else {
+                            console.error('В turan нет массива otchet или он пустой');
+                        }
+                    } else {
+                        console.error('Не найдены данные в turan');
+                    }
                     break;
                 case 'ilyas':
                     await schetfakturaFenixModel.findOneAndUpdate({ _id: item._id }, item, { upsert: true });
+                    const dataotchetfenix = await fenixOtchetBetaModel.find();
+                    if (dataotchet.length > 0) {
+                        const firstItem = dataotchetfenix[0];
+                        if (firstItem.otchet && firstItem.otchet.length > 0) {
+                            for (const obj of firstItem.otchet) {
+                                if (!obj.sity && !obj.admin && !obj.comPersent100) {
+                                    obj.sity = `${item.FIO} ${item.city}`;
+                                    obj.admin = item.admin;
+                                    obj.buyer = item.manager;
+                                    obj.comPersent100 = item.comission;
+                                }
+                            }
+                            await firstItem.save();
+                        } else {
+                            console.error('В turan нет массива otchet или он пустой');
+                        }
+                    } else {
+                        console.error('Не найдены данные в turan');
+                    }
                     break;
                 case 'yntymak':
                     await schetfakturaYntymakModel.findOneAndUpdate({ _id: item._id }, item, { upsert: true });
+                    const dataotchetfbox = await newOtdelOtchetBetaModel.find();
+                    if (dataotchet.length > 0) {
+                        const firstItem = dataotchetfbox[0];
+                        if (firstItem.otchet && firstItem.otchet.length > 0) {
+                            for (const obj of firstItem.otchet) {
+                                if (!obj.sity && !obj.admin && !obj.comPersent100) {
+                                    obj.sity = `${item.FIO} ${item.city}`;
+                                    obj.admin = item.admin;
+                                    obj.buyer = item.manager;
+                                    obj.comPersent100 = item.comission;
+                                }
+                            }
+                            await firstItem.save();
+                        } else {
+                            console.error('В turan нет массива otchet или он пустой');
+                        }
+                    } else {
+                        console.error('Не найдены данные в turan');
+                    }
                     break;
                 default:
                     console.error('Неизвестное значение team:', item.team);
@@ -87,4 +275,4 @@ const updateSchetSumma = async () => {
     }
 }
 
-export default { getSchetData, updateSchetData, updateSchetSumma, updateSchetTeam }
+export default { getSchetData, updateSchetData, updateSchetSumma, updateSchetTeam, getSchetDatafbox, getSchetDatafenix, getSchetDataleader, getSchetDataliberty, getSchetDatamonaco, getSchetDataturan }
--- src/routes/schetfactura/schetfactura.mjs
+++ src/routes/schetfactura/schetfactura.mjs
@@ -3,5 +3,11 @@ const router = express.Router();
 import getfactura from '../../controllers/telegram-bot/schet-control.mjs';
 
 router.get('/schetfactura', getfactura.getSchetData)
+router.get('/schetfactura-fbox', getfactura.getSchetDatafbox)
+router.get('/schetfactura-monaco', getfactura.getSchetDatamonaco)
+router.get('/schetfactura-turan', getfactura.getSchetDataturan)
+router.get('/schetfactura-fenix', getfactura.getSchetDatafenix)
+router.get('/schetfactura-liberty', getfactura.getSchetDataliberty)
+router.get('/schetfactura-leader', getfactura.getSchetDataleader)
 
 export default router
