3c0112f1753f96fa7532e96ea4fb2756092aeb53
albanovs <albvnovs@gmail.com>
2024-12-28 12:07:39 +0600

    model changes

--- src/models/MyModelForTg.js
+++ src/models/MyModelForTg.js
@@ -6,10 +6,7 @@ const myModelForTgSchema = new mongoose.Schema({
   num: Number,
   monako: String,
   fenix: String,
-  lider: String,
   turan: String,
-  liberty: String,
-  fbox: String
 });
 
 const MyModelForTg = mongoose.model('telegramslots', myModelForTgSchema);
